e21328261e0dd604eaa959ac784ba02fac2725d8
shinichi <shinichi@hotmail.nl>
2020-05-26 14:34:45 +0200

    added toast

--- app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
+++ app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class OpdrachtActivity extends AppCompatActivity {
 
@@ -19,14 +20,16 @@ public class OpdrachtActivity extends AppCompatActivity {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         bluetoothAvailable = (bluetoothAdapter != null);
 
-        if(bluetoothAvailable) {
+        if (bluetoothAvailable) {
             if (!bluetoothAdapter.isEnabled() && requestBluetooth()) {
-                
+
             }
+        } else {
+            Toast.makeText(this, "device doesn't support bluetooth", Toast.LENGTH_SHORT);
         }
     }
 
-    private boolean requestBluetooth(){
+    private boolean requestBluetooth() {
         Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(enableBluetooth, 0);
         return bluetoothAdapter.isEnabled();
