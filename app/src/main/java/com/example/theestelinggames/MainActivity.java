a60be75209693620631986c6f10f27bc83d908df
stijn <lemmstijn@hotmail.com>
2020-05-26 10:35:55 +0200

    edit

--- app/src/main/java/com/example/theestelinggames/MainActivity.java
+++ app/src/main/java/com/example/theestelinggames/MainActivity.java
@@ -8,21 +8,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BluetoothAdapter bluetoothAdapter;
-    private boolean bluetoothAvailable;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        bluetoothAvailable = (bluetoothAdapter != null);
-
-        if(bluetoothAvailable && !bluetoothAdapter.isEnabled()) {
-            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBluetooth, 0);
-        }
-
-
     }
 }
--- app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
+++ app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
@@ -2,13 +2,26 @@ package com.example.theestelinggames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class OpdrachtActivity extends AppCompatActivity {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private boolean bluetoothAvailable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opdracht);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAvailable = (bluetoothAdapter != null);
+
+        if(bluetoothAvailable && !bluetoothAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
     }
 }
