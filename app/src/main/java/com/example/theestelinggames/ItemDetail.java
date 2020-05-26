6875b723b250466a54e76a779c5a72c271b910f7
shinichi <shinichi@hotmail.nl>
2020-05-26 15:56:08 +0200

    moved bluetooth stuff to detail view

--- app/src/main/java/com/example/theestelinggames/ItemDetail.java
+++ app/src/main/java/com/example/theestelinggames/ItemDetail.java
@@ -2,15 +2,87 @@ package com.example.theestelinggames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class ItemDetail extends AppCompatActivity {
 
     public static final String ASSIGNMENT_ID = "AssignmentID";
 
+    private BluetoothAdapter bluetoothAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.assignment_item_detail);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    }
+
+    public void onConnectButtonClicked(View view) {
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "bluetooth not supported", Toast.LENGTH_SHORT).show();
+        } else {
+            if (bluetoothAdapter.isDiscovering()) {//opnieuw starten
+                bluetoothAdapter.cancelDiscovery();
+            }
+            checkBTPermissions();
+            if (bluetoothAdapter.startDiscovery()) {
+                //If discovery has started, then display the following toast....//
+                Toast.makeText(getApplicationContext(), "Discovering other bluetooth devices...",
+                        Toast.LENGTH_SHORT).show();
+                IntentFilter bluetoothActionFoundFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(broadcastReceiver, bluetoothActionFoundFilter);
+            } else {
+                //If discovery hasn’t started, then display this alternative toast//
+                Toast.makeText(getApplicationContext(), "Something went wrong! Discovery has failed to start.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.i("BLUETOOTH DEVICE FOUND", "DEVICE: " + device.getName());
+                makeToast("found device: " + device.getName());
+            }
+        }
+    };
+
+    public void makeToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+
+    /**
+     * This method is required for all devices running API23+
+     * Android must programmatically check the permissions for bluetooth. Putting the proper permissions
+     * in the manifest is not enough.
+     *
+     * NOTE: This will only execute on versions > LOLLIPOP because it is not needed otherwise.
+     */
+    private void checkBTPermissions() {
+        int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+        permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+        if (permissionCheck != 0) {
+
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+        }
     }
 }
--- app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
+++ app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
@@ -18,72 +18,11 @@ import android.widget.Toast;
 
 public class OpdrachtActivity extends AppCompatActivity {
 
-    private BluetoothAdapter bluetoothAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opdracht);
 
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-
     }
 
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                Log.i("BLUETOOTH DEVICE FOUND", "DEVICE: " + device.getName());
-                makeToast("found device: " + device.getName());
-            }
-        }
-    };
-
-    public void makeToast(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-    }
-
-
-    public void onScanButtonCLicked(View view) {
-        if (bluetoothAdapter == null) {
-            Toast.makeText(this, "bluetooth not supported", Toast.LENGTH_SHORT).show();
-        } else {
-            if (bluetoothAdapter.isDiscovering()) {//opnieuw starten
-                bluetoothAdapter.cancelDiscovery();
-            }
-            checkBTPermissions();
-            if (bluetoothAdapter.startDiscovery()) {
-                //If discovery has started, then display the following toast....//
-                Toast.makeText(getApplicationContext(), "Discovering other bluetooth devices...",
-                        Toast.LENGTH_SHORT).show();
-                IntentFilter bluetoothActionFoundFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                registerReceiver(broadcastReceiver, bluetoothActionFoundFilter);
-            } else {
-                //If discovery hasn’t started, then display this alternative toast//
-                Toast.makeText(getApplicationContext(), "Something went wrong! Discovery has failed to start.",
-                        Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-
-    /**
-     * This method is required for all devices running API23+
-     * Android must programmatically check the permissions for bluetooth. Putting the proper permissions
-     * in the manifest is not enough.
-     *
-     * NOTE: This will only execute on versions > LOLLIPOP because it is not needed otherwise.
-     */
-    private void checkBTPermissions() {
-        int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
-        permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
-        if (permissionCheck != 0) {
-
-            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
-        }
-    }
 }
