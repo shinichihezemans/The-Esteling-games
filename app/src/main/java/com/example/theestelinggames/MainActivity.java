aad9c7524ac9e910ff4ce77a08ec5afda1b21c16
shinichi <shinichi@hotmail.nl>
2020-05-26 10:40:38 +0200

    added OpdrachtActivity button on main activity

--- app/src/main/java/com/example/theestelinggames/MainActivity.java
+++ app/src/main/java/com/example/theestelinggames/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCLick(View view){
+        Intent intent = new Intent(this,OpdrachtActivity.class);
+        startActivity(intent);
     }
 }
