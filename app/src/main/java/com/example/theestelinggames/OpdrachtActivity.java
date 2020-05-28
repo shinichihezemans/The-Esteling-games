bdde35d12a986c47cd29d786a4844fcf476e53c8
shinichi <shinichi@hotmail.nl>
2020-05-28 10:40:07 +0200

    added button and textView

--- app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
+++ app/src/main/java/com/example/theestelinggames/OpdrachtActivity.java
@@ -14,15 +14,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class OpdrachtActivity extends AppCompatActivity {
+    private boolean isStarted = false;//if not started button visible and textView gone else button gone and textView visible
+
+    private TextView assignmentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opdracht);
-
+        assignmentTextView = findViewById(R.id.assignmentTextView);
+        assignmentTextView.setVisibility(View.GONE);
     }
 
+
+    public void onStartButtonClicked(View view) {
+        this.isStarted = true;
+        view.setVisibility(View.GONE);
+        assignmentTextView.setVisibility(View.VISIBLE);
+    }
 }
