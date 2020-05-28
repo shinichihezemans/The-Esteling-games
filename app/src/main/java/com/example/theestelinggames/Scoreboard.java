c1426f4b8974582aab82500f609b661652404d6b
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-05-28 16:10:32 +0200

    Made Scoreboard activity and class

---
+++ app/src/main/java/com/example/theestelinggames/Scoreboard.java
@@ -0,0 +1,50 @@
+package com.example.theestelinggames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class Scoreboard extends AppCompatActivity {
+
+    MqttAndroidClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoreboard);
+
+        String clientId = MqttClient.generateClientId();
+
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://maxwell.bps-software.nl:1883", clientId);
+
+        try{
+
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setUserName("androidTI");
+            options.setPassword("&FN+g$$Qhm7j".toCharArray());
+            IMqttToken token = client.connect(options);
+
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i("Succes", "Connected");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i("Failure", "Failed to connect");
+                }
+            });
+        }catch (MqttException e){
+            e.printStackTrace();
+        }
+    }
+}
